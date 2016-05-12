c9ae42007396f04145495e72cad526e94ce0feda
Philipp MACMINI <philipp.seifert@iguanasell.com>
2016-05-12 18:54:18 +0200

    Added Comment 6

--- GIT_and_GitHub_Tutorial/Form1.cs
+++ GIT_and_GitHub_Tutorial/Form1.cs
@@ -25,6 +25,7 @@ namespace GIT_and_GitHub_Tutorial
             //Adding Comment 3 MacMini 18:37h
             //Adding Comment 4 Philipp Work PC 18:43h
             //Comment 5
+            //Comment 6
         }
     }
 }
